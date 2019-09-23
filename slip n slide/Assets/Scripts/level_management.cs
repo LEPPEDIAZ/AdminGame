3ea7704bbfc365a102c774ddcfd9dd28f43d0a2e
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-09-23 13:29:51 -0600

    changes

---
+++ slip n slide/Assets/Scripts/level_management.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level_management : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void carga_scena()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
